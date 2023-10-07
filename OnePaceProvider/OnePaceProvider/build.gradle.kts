a192d6dc8b53d8cc34ac5411d12f16f84f08544d
KillerDogeEmpire <edison.beckett@gmail.com>
2023-10-07 16:12:27 -0700

    add one pace

---
+++ OnePaceProvider/OnePaceProvider/build.gradle.kts
@@ -0,0 +1,24 @@
+version = 1
+
+
+cloudstream {
+    language = "en"
+    // All of these properties are optional, you can safely remove them
+
+    description = "One Pace"
+    authors = listOf("KillerDogeEmpire")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "Anime"
+    )
+
+    iconUrl = "https://raw.githubusercontent.com/KillerDogeEmpire/avocado-extensions/master/logos/onepace.png"
+}
