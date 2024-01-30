4f05703cca23e4624dc7e479283e2f984ae3f3a8
rushi-chavan <rushikeshchavan9887@gmail.com>
2024-01-30 06:46:56 -0800

    New Aniwave (Simkl) exrension

---
+++ AniwaveSimklProvider/build.gradle.kts
@@ -0,0 +1,26 @@
+// use an integer for version numbers
+version = 38
+
+
+cloudstream {
+    language = "en"
+    // All of these properties are optional, you can safely remove them
+
+    description = "Watch Aniwave/9anime, I have had reports saying homepage doesn't work the first time but retrying should fix it"
+    authors = listOf("Horis, Stormunblessed, KillerDogeEmpire, Enimax, Chokerman")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "Anime",
+        "OVA",
+    )
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=aniwave.to&sz=%size%"
+}
