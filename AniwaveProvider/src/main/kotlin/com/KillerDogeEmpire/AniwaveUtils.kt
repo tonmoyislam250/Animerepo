ff1ae6e929f45cabf45f0d7fdef82836c31d9c23
Tonmoy Islam <tonmoyk983@gmail.com>
2024-04-05 21:48:22 +0600

    Update AniwaveUtils.kt

--- AniwaveProvider/src/main/kotlin/com/KillerDogeEmpire/AniwaveUtils.kt
+++ AniwaveProvider/src/main/kotlin/com/KillerDogeEmpire/AniwaveUtils.kt
@@ -1,15 +1,15 @@
 package com.KillerDogeEmpire
 
 import android.util.Base64
+import com.lagradost.cloudstream3.syncproviders.SyncIdName
 import java.net.URLDecoder
 import javax.crypto.Cipher
 import javax.crypto.spec.SecretKeySpec
-import com.lagradost.cloudstream3.syncproviders.SyncIdName
 
 object AniwaveUtils {
 
     fun vrfEncrypt(input: String): String {
-        val rc4Key = SecretKeySpec("ysJhV6U27FVIjjuk".toByteArray(), "RC4")
+        val rc4Key = SecretKeySpec("tGn6kIpVXBEUmqjD".toByteArray(), "RC4")
         val cipher = Cipher.getInstance("RC4")
         cipher.init(Cipher.DECRYPT_MODE, rc4Key, cipher.parameters)
 
@@ -17,8 +17,9 @@ object AniwaveUtils {
         vrf = Base64.encode(vrf, Base64.URL_SAFE or Base64.NO_WRAP)
         vrf = Base64.encode(vrf, Base64.DEFAULT or Base64.NO_WRAP)
         vrf = vrfShift(vrf)
-        vrf = Base64.encode(vrf, Base64.DEFAULT)
-        vrf = rot13(vrf)
+        // vrf = rot13(vrf)
+        vrf = vrf.reversed().toByteArray()
+        vrf = Base64.encode(vrf, Base64.URL_SAFE or Base64.NO_WRAP)
         val stringVrf = vrf.toString(Charsets.UTF_8)
         return "vrf=${java.net.URLEncoder.encode(stringVrf, "utf-8")}"
     }
@@ -27,7 +28,7 @@ object AniwaveUtils {
         var vrf = input.toByteArray()
         vrf = Base64.decode(vrf, Base64.URL_SAFE)
 
-        val rc4Key = SecretKeySpec("hlPeNwkncH0fq9so".toByteArray(), "RC4")
+        val rc4Key = SecretKeySpec("LUyDrL4qIxtIxOGs".toByteArray(), "RC4")
         val cipher = Cipher.getInstance("RC4")
         cipher.init(Cipher.DECRYPT_MODE, rc4Key, cipher.parameters)
         vrf = cipher.doFinal(vrf)
@@ -49,20 +50,21 @@ object AniwaveUtils {
 
     private fun vrfShift(vrf: ByteArray): ByteArray {
         for (i in vrf.indices) {
-            val shift = arrayOf(-3, 3, -4, 2, -2, 5, 4, 5)[i % 8]
+            val shift = arrayOf(-2, -4, -5, 6, 2, -3, 3, 6)[i % 8]
             vrf[i] = vrf[i].plus(shift).toByte()
         }
         return vrf
     }
 
     fun aniQuery(name: SyncIdName, id: Int): String {
-        //creating query for Anilist API using ID
-        val idType = when (name) {
-            SyncIdName.MyAnimeList -> "idMal"
-            else -> "id"
-
-        }
-        val query = """
+        // creating query for Anilist API using ID
+        val idType =
+                when (name) {
+                    SyncIdName.MyAnimeList -> "idMal"
+                    else -> "id"
+                }
+        val query =
+                """
             query {
                 Media($idType: $id, type: ANIME) {
                     title {
@@ -80,8 +82,9 @@ object AniwaveUtils {
     }
 
     fun aniQuery(titleRomaji: String, year: Int, season: String): String {
-        //creating query for Anilist API using name and other details
-        val query = """
+        // creating query for Anilist API using name and other details
+        val query =
+                """
             query {
                 Media(search: "$titleRomaji", season:${season.uppercase()}, seasonYear:$year, type: ANIME) {
                     title {
